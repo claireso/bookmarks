e90091f2eba0ae73085dc8edaf3a94056cb4b020
Claire Sosset <claire.sosset@gmail.com>
2014-08-26 01:32:24 +0200

    clean browserify task

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -5,21 +5,12 @@ var gulp = require('gulp'),
     source = require('vinyl-source-stream');
 
 gulp.task('browserify', ['clean'], function() {
-    //app.js
-    // return gulp.src(paths.scripts + '*.js')
-    //     .pipe(concat('app.js', {newLine: ';'}))
-    //     .pipe(browserify({
-    //       insertGlobals : true,
-    //       debug: true
-    //     }))
-    //     .pipe(gulp.dest(paths.dist));
-    //     
+
     var b = browserify('./' + paths.scripts + "base.js", {
         debug: true
     });
 
     return b.bundle()
     .pipe(source('app.js'))
-    // .pipe(argv.env != "production" ? gutil.noop() : gStreamify(uglify()))
     .pipe(gulp.dest(paths.dist));
 });
