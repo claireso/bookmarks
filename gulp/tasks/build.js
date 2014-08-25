5be5b99a71b030407a9f5db734639ff1da871940
Claire Sosset <claire.sosset@gmail.com>
2014-08-26 01:28:47 +0200

    fix gulp task build

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     paths = require('../paths.js'),
     uglify = require('gulp-uglify');
 
-gulp.task('build', ['concat'], function() {
+gulp.task('build', ['browserify'], function() {
     return gulp.src([paths.dist + '**/*.js'])
     .pipe(uglify())
     .pipe(gulp.dest(paths.dist));
